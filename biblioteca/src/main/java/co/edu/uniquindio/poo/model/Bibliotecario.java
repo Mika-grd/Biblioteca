cbfd2fafc267132a9bbd0f9ca64d092368bdca70
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2024-10-06 19:39:17 +0000

    L

--- biblioteca/src/main/java/co/edu/uniquindio/poo/model/Bibliotecario.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/model/Bibliotecario.java
@@ -1,61 +1,17 @@
 package co.edu.uniquindio.poo.model;
 
-import java.util.LinkedList;
-import java.time.LocalDate;
 
-public class Bibliotecario {
+public class Bibliotecario extends Persona{
 
-    private String nombre;
-    private String id;
-    private String telefono;
-    private String correo;
     private double salario;
-    private LocalDate añoContratacion;
-    private LinkedList<Prestamo> listaPrestamosDeUnBibliotecario;
 
 
-    public Bibliotecario(String nombre, String id, String telefono, String correo, double salario, LocalDate añoContratacion){
-        this.nombre = nombre;
-        this.id = id;
-        this.telefono = telefono;
-        this.correo = correo;
+    public Bibliotecario(String nombre, String id, String telefono, String correo, double salario){
+        super(nombre, id, telefono, correo);
         this.salario = salario;
-        this.añoContratacion = añoContratacion;
-        this.listaPrestamosDeUnBibliotecario =  new LinkedList<Prestamo>();
-        
     }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
 
     public double getSalario() {
         return salario;
@@ -65,20 +21,4 @@ public class Bibliotecario {
         this.salario = salario;
     }
 
-    public LinkedList<Prestamo> getListaPrestamosDeUnBibliotecario() {
-        return listaPrestamosDeUnBibliotecario;
-    }
-
-    public void setListaPrestamosDeUnBibliotecario(LinkedList<Prestamo> listaPrestamosDeUnBibliotecario) {
-        this.listaPrestamosDeUnBibliotecario = listaPrestamosDeUnBibliotecario;
-    }
-
-    public LocalDate getAñoContratacion() {
-        return añoContratacion;
-    }
-
-    public void setAñoContratacion(LocalDate añoContratacion) {
-        this.añoContratacion = añoContratacion;
-    }
-    
 }
