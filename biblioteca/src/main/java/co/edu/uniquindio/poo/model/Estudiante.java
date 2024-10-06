9cf809348f32144a0a8241b5864b4307620b9ecd
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2024-10-06 16:10:04 -0500

    Delete biblioteca directory

--- biblioteca/src/main/java/co/edu/uniquindio/poo/model/Estudiante.java
+++
@@ -1,8 +0,0 @@
-public class Estudiante extends Persona {
-
-
-    public Estudiante(String nombre, String id, String telefono, String correo){
-        super(nombre, id, telefono, correo);
-
-    }
-}
